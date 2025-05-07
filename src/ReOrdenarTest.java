d975f867b980d501afdc442027c93b79604076e2
tarde <joelnfjoel@gmail.com>
2025-05-07 16:18:55 +0100

    Refactorizar e implementar nuevos test

--- src/ReOrdenarTest.java
+++ src/ReOrdenarTest.java
@@ -6,20 +6,41 @@ public class ReOrdenarTest {
 
     @Test
     public void test_01() {
-        int entrada = 42145;
-        int salidaEsperada = 54421;
-        assertEquals(salidaEsperada, ReOrdenar.ordenarDesc(entrada));
+        assertEquals(54421, ReOrdenar.ordenarDesc(42145));
     }
     @Test
     public void test_02() {
-        int entrada = 145263;
-        int salidaEsperada = 654321;
-        assertEquals(salidaEsperada, ReOrdenar.ordenarDesc(entrada));
+        assertEquals(654321, ReOrdenar.ordenarDesc(145263));
     }
     @Test
     public void test_03() {
-        int entrada = 123456789;
-        int salidaEsperada = 987654321;
-        assertEquals(salidaEsperada, ReOrdenar.ordenarDesc(entrada));
+        assertEquals(987654321, ReOrdenar.ordenarDesc(123456789));
     }
+
+    @Test
+    public void test_04() {
+        assertEquals(0, ReOrdenar.ordenarDesc(0));
+    }
+
+    @Test
+    public void test_05() {
+        assertEquals(4, ReOrdenar.ordenarDesc(4));
+    }
+
+    @Test
+    public void test_06() {
+        assertEquals(9654310, ReOrdenar.ordenarDesc(9654310));
+    }
+
+    @Test
+    public void test_07() {
+        assertEquals(11110000, ReOrdenar.ordenarDesc(10101100));
+    }
+
+    @Test
+    public void test_08() {
+        assertEquals(01001011, ReOrdenar.ordenarDesc(01001011));
+    }
+
+
 }
